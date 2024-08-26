fe8115ef6b108cfb0fbe25730cd277bec8fe097f
AshleyEvonne <ashleyevonne345@gmail.com>
2024-08-26 11:11:27 -0400

    Model, ModelAndView, and @ModelAttribute annotation

---
+++ GLAB309.5.3/src/test/java/com/java/GLAB30953/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.GLAB30953;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
